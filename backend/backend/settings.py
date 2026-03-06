865825468312c6bbe2ce4dc859092c644d7bfb7e
Devkaran-Patidar <pdevkaran023@gmail.com>
2026-03-06 23:21:19 +0530

    remove sqlite DataBase

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -113,12 +113,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 import dj_database_url
 from dotenv import load_dotenv
